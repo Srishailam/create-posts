f81c6db68ce1190e7fea51bc391b9d45da52f6a8
Srishailam Dasari <srishailam.dasari@uipath.com>
2020-06-19 16:03:53 -0400

    Updates

---
+++ precache-manifest.e015abc18120ea58cebd5d6901f306dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae499e61b7e4212da51edc2543f7085",
+    "url": "/create-posts/index.html"
+  },
+  {
+    "revision": "d68864fa48097ee4fee8",
+    "url": "/create-posts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "029d57e39aeb073c7eb7",
+    "url": "/create-posts/static/js/2.ecf758f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-posts/static/js/2.ecf758f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68864fa48097ee4fee8",
+    "url": "/create-posts/static/js/main.a7c7a9c8.chunk.js"
+  },
+  {
+    "revision": "2f5d5ea3fa3ed7176841",
+    "url": "/create-posts/static/js/runtime-main.f8d7c210.js"
+  }
+]);
